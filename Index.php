ca2e217e456d4ddf371d5d49d8fc7df544df3b5c
FlorianePuy <77892356+FlorianePuy@users.noreply.github.com>
2024-02-24 14:38:43 +0100

    début du projet

---
+++ Index.php
@@ -0,0 +1,11 @@
+<!doctype html>
+<html lang="fr-FR">
+<head>
+    <meta charset="utf-8">
+    <title>Pokédex</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <nav> </nav>
+</body>
+</html>
