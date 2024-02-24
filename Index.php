d42ed475680a447c1ea69f5c4b67c2bb0411ff61
FlorianePuy <77892356+FlorianePuy@users.noreply.github.com>
2024-02-24 16:33:15 +0100

    ajout de la nav barre, d'une card exemple et un peu de css

--- Index.php
+++ Index.php
@@ -4,8 +4,29 @@
     <meta charset="utf-8">
     <title>Pokédex</title>
     <link rel="stylesheet" href="style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Rubik:ital,wght@0,300..900;1,300..900&display=swap" rel="stylesheet">
 </head>
 <body>
-    <nav> </nav>
+    <nav>
+        <div class="logo-pokeball">
+        </div>
+        <form action="">
+        <input id="search" name="search" type="search" placeholder="Recherche...">
+        <button type="submit">Rechercher</button>
+        </form>
+    </nav>
+    <div class="container">
+        <div class="card">
+            <img src="Assets/Salameche.png" alt="image de salamèche">
+            <h3 class="name">Salamèche</h3>
+            <p class="description">La flamme qui brûle au bout de sa queue indique l'humeur de ce Pokémon.
+                Elle vacille lorsque Salamèche est content. En revanche, lorsqu'il s'énerve, la flamme 
+                prend de l'importance et brûle plus ardemment.</p>
+            <p>Type :</p>
+            <p>Numéro :</p>
+        </div>
+    </div>
 </body>
 </html>
