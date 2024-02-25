13dbafc978aa8586ce33918be625ca6d2fbb0632
FlorianePuy <77892356+FlorianePuy@users.noreply.github.com>
2024-02-25 22:21:59 +0100

    ajout de la classe Type

--- Type.php
+++ Type.php
@@ -1,5 +1,60 @@
 <?php
 
 class Type {
+    private $id;
+    private $name;
+    private $color;
 
+    /**
+     * @return mixed
+     */
+    public function getName() {
+        return $this->name;
+    }
+
+    /**
+     * @param mixed $id
+     */
+    public function setId($id) {
+        $this->id = $id;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getId() {
+        return $this->id;
+    }
+
+    /**
+     * @param mixed $name
+     */
+    public function setName($name) {
+        $this->name = $name;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getColor() {
+        return $this->color;
+    }
+
+    /**
+     * @param mixed $color
+     */
+    public function setColor($color) {
+        $this->color = $color;
+    }
+    public function hydrate($data) {
+        foreach ($data as $key => $value) {
+            $method = "set".ucfirst($key);
+            if(method_exists($this,$method)){
+                $this->$method($value);
+            }
+        }
+    }
+    public function __construct($data) {
+        $this->hydrate($data);
+    }
 }
