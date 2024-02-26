ee8c665b611ff841a14e600c14afe825cdfeec80
FlorianePuy <77892356+FlorianePuy@users.noreply.github.com>
2024-02-26 20:51:20 +0100

    création du controleur (MVC) avec le CRUD a finir d'implémmenter

--- PokemonsManager.php
+++ PokemonsManager.php
@@ -1,5 +1,64 @@
 <?php
 
 class PokemonsManager {
+    private $db;
+    public function __construct() {
+        $dbName="pokedex";
+        $port="3306";
+        $username="root";
+        $password="";
+        try {
+            $this->db=new PDO('mysql:host=localhost;dbname=$dbName;port=$port',$username,$password);
+        }catch (PDOException $exception){
+            echo $exception->getMessage();
+        }
+    }
+    public function create(Pokemon $pokemon):void {
+        $req=$this->db->prepare("INSERT INTO pokemon (number,name,description,type1,type2,url_image) VALUES (:number,:name,:description,:type1,:type2,:url_image);");
+        $req->bindValue(":number",$pokemon->getNumber(),PDO::PARAM_INT);
+        $req->bindValue(":name",$pokemon->getName(),PDO::PARAM_STR);
+        $req->bindValue(":description",$pokemon->getDescription(),PDO::PARAM_STR);
+        $req->bindValue(":type1",$pokemon->getType1(),PDO::PARAM_STR);
+        $req->bindValue(":type2",$pokemon->getType2(),PDO::PARAM_STR);
+        $req->bindValue(":url_image",$pokemon->getUrl_image(),PDO::PARAM_STR);
+
+        $req->execute();
+    }
+    public function get($id):Pokemon{
+        $req=$this->db->prepare("SELECT * FROM pokemon WHERE id=:id");
+        $req->bindValue(":id",$id,PDO::PARAM_INT);
+        $data=$req->fetch();
+        return new Pokemon($data);
+    }
+    public function getAll():array {
+        $pokemons=[];
+        $req=$this->db->query("SELECT * FROM pokemon ORDER BY number");
+        $datas=$req->fetchAll();
+        foreach ($datas as $data){
+            $pokemon=new Pokemon($data);
+            $pokemons[] = $pokemon;
+        }
+        return $pokemons;
+    }
+    public function getAllByString(string $input) {
+        $pokemons=[];
+        $req=$this->db->prepare("SELECT * FROM pokemon WHERE name LIKE :name ORDER BY number");
+        $req->bindValue(":name",$input,PDO::PARAM_STR);
+        $datas=$req->fetchAll();
+        foreach ($datas as $data){
+            $pokemon=new Pokemon($data);
+            $pokemons[] = $pokemon;
+        }
+        return $pokemons;
+    }
+    public function getAllByType(string $input) {
+
+    }
+    public function update() {
+
+    }
+    public function delete() {
+
+    }
 
 }
